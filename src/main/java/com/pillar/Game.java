f00a73a1e958f4f85a23de7b9a3dc96ca1ea7f80
Jeremy Anderson <jeremy.d.anderson@gmail.com>
2010-10-05 20:00:11 -0400

    Added a game object

---
+++ src/main/java/com/pillar/Game.java
@@ -0,0 +1,11 @@
+package com.pillar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jeremy
+ * Date: Oct 5, 2010
+ * Time: 7:59:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Game {
+}
---
+++ src/test/java/com/pillar/GameTest.java
@@ -0,0 +1,11 @@
+package com.pillar;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jeremy
+ * Date: Oct 5, 2010
+ * Time: 7:59:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameTest {
+}
